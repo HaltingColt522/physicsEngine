1369d429742b117c8c4af938984e102cc0a901a1
HaltingColt522 <l.frotschi@web.de>
2024-01-16 20:42:42 +0100

    deleted useless data

--- game/draw_triangle.cpp
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-void draw_triangle(unsigned int shaderProgram, GLuint vertexBuffer) {
-    /// draw our first triangle
-    glUseProgram(shaderProgram);
-    glBindVertexArray(vertexBuffer); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-}
